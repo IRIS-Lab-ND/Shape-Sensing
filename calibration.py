76d644cff8bd4e4fffc9f1ac5c654d57ae9b2102
Kabir Madan <kabirmadan@dhcp-10-5-120-58.dhcp.nd.edu>
2022-06-28 15:43:51 -0400

    added automatic calibration with aruco marker detection

--- calibration.py
+++ calibration.py
@@ -1,9 +1,91 @@
-import cv2
+#imports
+from scipy.spatial.distance import cdist
 import numpy as np
+import imutils
+from imutils import contours
+from imutils import perspective
+import cv2
 
+
+
+filename = 'media/video.mp4'
+pixels_per_cm = 0
+
+
+
+#-----AUTOMATIC CALIBRATION USING ARUCO MARKER-----#
+#code largely taken from https://arshren.medium.com/measure-object-size-using-opencv-and-aruco-marker-fa8b2e3b0572
+
+def findArucoMarker(image, markerSize=5, totalMarkers=100, draw=True):
+	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	key = getattr(cv2.aruco, f'DICT_{markerSize}X{markerSize}_{totalMarkers}')
+
+	arucoDict = cv2.aruco.Dictionary_get(key)
+
+	arucoParam = cv2.aruco.DetectorParameters_create()
+
+	bboxs, idx, rejected = cv2.aruco.detectMarkers(gray, arucoDict, parameters=arucoParam)
+
+	return bboxs, idx, rejected
+
+
+def automaticallyCalibrate():
+
+	global pixels_per_cm
+
+	# dimensions of aruco printout are 7x7 cm so perimeter is 7*4 cm
+	true_aruco_perimeter = 7 * 4
+
+	cap = cv2.VideoCapture(filename)
+	ret, image = cap.read()
+
+	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	gray = cv2.GaussianBlur(gray, (7,7), 0)
+
+	edged = cv2.Canny(gray, 50, 100)
+	edged = cv2.dilate(edged, None, iterations=1)
+	edged = cv2.erode(edged, None, iterations=1)
+
+	cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+	cnts = imutils.grab_contours(cnts)
+	(cnts, _) = contours.sort_contours(cnts)
+
+	arucofound = findArucoMarker(image, totalMarkers=100)
+		
+	vertices = []
+
+	for i in range(4):
+		point = []
+		curr_x = arucofound[0][0][0][i][0]
+		curr_y = arucofound[0][0][0][i][1]
+		point.append(curr_x)
+		point.append(curr_y)
+		vertices.append(point)
+
+	vertices = np.asarray(vertices, dtype=np.int32)
+	print(vertices)
+	np_pts = vertices.reshape((-1,1,2))
+	cv2.polylines(image, [np_pts], isClosed=True, color=(255,255,0), thickness=5)
+
+	if len(arucofound[0])!=0:
+		virtual_aruco_perimeter = cv2.arcLength(arucofound[0][0][0], True)
+		print("marker perimeter: {} pixels".format(virtual_aruco_perimeter))
+		pixels_per_cm  = virtual_aruco_perimeter / true_aruco_perimeter
+		print("{} pixels per cm".format(pixels_per_cm))
+		return 1
+	else:
+		print("Automatic calibration failed")
+		return 0
+
+
+
+
+
+#-----MANUAL CALIBRATION-----#
 
 all_points = []
-
+cap = cv2.VideoCapture(filename)
+ret, image = cap.read()
 
 def selectPoints(event, x, y, flags, param):
 	global all_points
@@ -12,10 +94,10 @@ def selectPoints(event, x, y, flags, param):
 		point.append(x)
 		point.append(y)
 		all_points.append(point)
-		cv2.circle(frame, (x,y), 10, (0,255,0), 10)
-		#print(all_points)
+		cv2.circle(image, (x,y), 10, (0,255,0), 10)
 
 
+#calculates the average distance between each consecutive pair of labeled points 
 def getAvgDist(all_points):
 	all_dists = []
 	num_points = len(all_points)
@@ -28,22 +110,32 @@ def getAvgDist(all_points):
 	return avg_distance
 
 
-cap = cv2.VideoCapture('media/video.mp4')
-ret, frame = cap.read()
-cv2.namedWindow(winname='calibration')
-cv2.setMouseCallback('calibration', selectPoints)
+def manuallyCalibrate():
+	global pixels_per_cm
+	cv2.namedWindow(winname='calibration')
+	cv2.setMouseCallback('calibration', selectPoints)
+	while True:
+		cv2.imshow('calibration', image)
+		if cv2.waitKey(1) & 0xFF == 27:
+			break
+	pixels_per_cm = getAvgDist(all_points)
+	cv2.destroyAllWindows()
 
 
-while True:
-	cv2.imshow('calibration', frame)
-
-	if cv2.waitKey(1) & 0xFF == 27:
-		break
 
 
-pixels_to_cm_conversion_factor = 1 / getAvgDist(all_points)
-print("{} pixels per cm".format(1 / pixels_to_cm_conversion_factor))
-print("{} cm per pixel".format(pixels_to_cm_conversion_factor))
 
-cv2.destroyAllWindows()
 
+
+#-----MAIN DRIVER-----#
+def calibrate():
+	try:
+		automaticallyCalibrate()
+	except:
+		print("Automatic calibration failed")
+		print("Opening manual calibration")
+		manuallyCalibrate()
+	print("CALIBRATION COMPLETE: {} pixels per cm.".format(pixels_per_cm))
+
+
+calibrate()
